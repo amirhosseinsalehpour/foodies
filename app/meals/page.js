68f286dce41bef9f333d0e76f19f8694907b7480
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-02-13 18:40:30 +0330

    create header and style

---
+++ app/meals/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+  return (
+    <div>
+      <h1>MealsPage</h1>
+      <Link href="/meals/share">share</Link>
+    </div>
+  );
+}
