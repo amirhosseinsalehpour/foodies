aae374e2621804677b062bb7d6ea2e5c1e1c98f8
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-02-17 11:32:16 +0330

    using use client

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,6 +3,8 @@ import LogoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+
 export default function MainHeader() {
   return (
     <>
@@ -16,10 +18,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browser Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
