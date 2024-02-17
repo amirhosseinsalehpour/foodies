d8d89cb6891cb326f1fb6b65c960672861b38047
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-02-17 16:43:21 +0330

    handle error in meal

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>And error occurred!</h1>
+      <p>Failed Fetching. Please try again!</p>
+    </main>
+  );
+}
